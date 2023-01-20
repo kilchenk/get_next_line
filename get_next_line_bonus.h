fead7b5fdbdeb71dce10018b39ae3ca4989b5f98
Kirill Ilchenko <kilchenk@c2s8d28.42wolfsburg.de>
2023-01-20 16:57:12 +0100

    ok

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,34 @@
 /*   By: kilchenk <kilchenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 19:41:06 by kilchenk          #+#    #+#             */
-/*   Updated: 2023/01/19 19:41:07 by kilchenk         ###   ########.fr       */
+/*   Updated: 2023/01/20 16:52:11 by kilchenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+//library
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdio.h>
+# include <fcntl.h>
+//functions
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*strjoin_free(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*trim_start(char *str);
+char	*trim_end(char *str);
+char	*ft_strdup(const char *s);
+
+#endif
